2758b04d10714970f9a3bfaaa45cd6695f3844c2
Antonis Zisis <antonis.zisis.dev@gmail.com>
2024-05-03 19:24:39 +0300

    feat: introduce basic home page (#1)

--- app/root.tsx
+++ app/root.tsx
@@ -4,22 +4,31 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  type MetaFunction,
 } from '@remix-run/react';
 
-import './globals.css';
+import './tailwind.css';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Antonis Zisis | Senior Full Stack Software Engineer' },
+    { name: 'author', content: 'Antonis Zisis' },
+    { name: 'description', content: "Antonis Zisis' personal website" },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ];
+};
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <Meta />
         <Links />
       </head>
 
-      <body>
+      <body className="bg-slate-50 text-slate-600 antialiased dark:bg-slate-900 dark:text-slate-200">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,22 @@
-import type { MetaFunction } from '@remix-run/react';
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: 'Antonis Zisis - Software Engineer' },
-    { name: 'description', content: "Antonis Zisis' personal website" },
-  ];
-};
-
 export default function Index() {
-  return <h1 className="flex items-center justify-center">Hello world!</h1>;
+  return (
+    <div className="mx-auto w-full max-w-screen-lg px-4 pt-20 sm:px-6 md:px-8">
+      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2">
+        <div>
+          <h1 className="text-5xl font-semibold">Antonis Zisis</h1>
+          <h2 className="mt-2 text-lg font-medium">
+            Senior Full Stack Software Engineer
+          </h2>
+
+          <div className="my-5 border-b border-slate-800" />
+
+          <div className="text-base text-slate-400">
+            <h3>Currently working at Causaly</h3>
+          </div>
+        </div>
+
+        <div></div>
+      </div>
+    </div>
+  );
 }
