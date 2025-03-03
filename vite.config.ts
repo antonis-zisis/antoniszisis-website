ff5ca6c35b5bc3f537d6238b6b449f01ed24d42a
Antonis <antonis.zisis.dev@gmail.com>
2025-03-03 22:26:55 +0200

    chore: configure deployment with netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-// import { netlifyPlugin } from '@netlify/remix-edge-adapter/plugin';
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  // @ts-expect-error there is an issue with "tailwindcss" types
-  plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
+  // @ts-expect-error there is an issue with "tailwindcss" and "netlifyPlugin" types
+  plugins: [reactRouter(), tsconfigPaths(), tailwindcss(), netlifyPlugin()],
 });
