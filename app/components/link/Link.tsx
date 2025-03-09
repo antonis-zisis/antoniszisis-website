1912228355bc29f34fb6331cc590d47257477e03
Antonis <antonis.zisis.dev@gmail.com>
2025-03-09 12:49:29 +0200

    feat: add animation for underline text

--- app/components/link/Link.tsx
+++ app/components/link/Link.tsx
@@ -1,16 +1,21 @@
+import cn from 'classnames';
 import type { ReactNode } from 'react';
 import { Link as RRLink } from 'react-router';
 
 type Props = {
   children: ReactNode;
+  className?: string;
   openInNewTab?: boolean;
   to: string;
 };
 
-export function Link({ children, openInNewTab = false, to }: Props) {
+export function Link({ children, className, openInNewTab = false, to }: Props) {
   return (
     <RRLink
-      className="text-slate-600 hover:text-red-800 dark:text-slate-200 dark:hover:text-teal-300"
+      className={cn(
+        'text-slate-600 hover:text-red-800 dark:text-slate-200 dark:hover:text-teal-300',
+        className
+      )}
       to={to}
       target={openInNewTab ? '_blank' : undefined}
       rel={openInNewTab ? 'noopener noreferrer' : undefined}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,9 +6,10 @@ import { Accent } from '@/components/typography';
 
 function CompanyText() {
   return (
-    <Link openInNewTab to="https://kaizengaming.com/home">
-      <span className="text-red-800 hover:underline dark:text-teal-300">
+    <Link className="group" openInNewTab to="https://kaizengaming.com/home">
+      <span className="relative text-red-800 dark:text-teal-300">
         Kaizen
+        <span className="absolute bottom-0 left-0 h-[1px] w-0 bg-red-800 transition-all duration-300 group-hover:w-full dark:bg-teal-300" />
       </span>
     </Link>
   );
