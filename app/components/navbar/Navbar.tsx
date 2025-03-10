013f7f6d0ada091e80ad5e7e523873e5d2fa23c6
Antonis <antonis.zisis.dev@gmail.com>
2025-03-10 11:35:56 +0200

    feat: add navigation links and setup active link

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -3,6 +3,18 @@ import { Link } from '@/components/link';
 import { Logo } from '@/components/logo';
 import { ThemeToggle } from '@/components/theme-toggle';
 
+type AppLink = {
+  id: string;
+  text: string;
+  url: string;
+};
+
+const links = [
+  { id: 'home', text: 'Home', url: '/' },
+  { id: 'about', text: 'About', url: '/about' },
+  { id: 'timeline', text: 'Timeline', url: '/timeline' },
+] satisfies Array<AppLink>;
+
 export function Navbar() {
   return (
     <header className="fixed top-0 z-10 flex h-20 w-full items-center justify-between px-20 py-0">
@@ -16,6 +28,14 @@ export function Navbar() {
         </div>
 
         <div className="flex items-center justify-center">
+          <div className="mr-8 flex gap-x-8 font-mono text-base">
+            {links.map((link) => (
+              <Link key={link.id} to={link.url}>
+                {link.text}
+              </Link>
+            ))}
+          </div>
+
           <ThemeToggle />
         </div>
       </nav>
---
+++ app/routes/about.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="mx-auto flex min-h-screen max-w-(--breakpoint-lg) flex-col items-center justify-center">
+      <h1 className="font-mono text-6xl text-red-800 dark:text-teal-300">
+        Coming Soon!
+      </h1>
+    </div>
+  );
+}
