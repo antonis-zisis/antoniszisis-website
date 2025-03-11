ce66ce2efb67324ff50c35cfc4002b558c7e4841
Antonis <antonis.zisis.dev@gmail.com>
2025-03-11 23:19:40 +0200

    feat: setup the layout for the timeline

--- app/routes/timeline.tsx
+++ app/routes/timeline.tsx
@@ -1,9 +1,23 @@
+import { Divider } from '@/components/divider';
+
+// const timelineData = [
+//   { year: '2024', title: 'Software Engineer', company: 'Company A' },
+//   { year: '2023', title: 'Frontend Developer', company: 'Company B' },
+//   { year: '2022', title: 'Intern', company: 'Company C' },
+// ];
+
 export default function Timeline() {
   return (
-    <div className="mx-auto flex min-h-screen max-w-(--breakpoint-lg) flex-col items-center justify-center">
-      <h1 className="font-mono text-6xl text-red-800 dark:text-teal-300">
-        Coming Soon!
+    <div className="mx-auto flex min-h-screen max-w-(--breakpoint-lg) flex-col items-start">
+      <h1 className="pt-28 text-3xl font-semibold text-slate-600 dark:text-slate-200">
+        Timeline
       </h1>
+
+      <h2 className="text-lg font-medium text-slate-600 dark:text-slate-400">
+        My story in one page
+      </h2>
+
+      <Divider />
     </div>
   );
 }
