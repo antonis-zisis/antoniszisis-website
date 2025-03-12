d7d08104ed6e7d807d1f9c59c3e9429ad2cd0d53
Antonis <antonis.zisis.dev@gmail.com>
2025-03-12 17:25:42 +0200

    feat: add an arrow on the timeline vertical line

--- app/routes/timeline.tsx
+++ app/routes/timeline.tsx
@@ -16,7 +16,22 @@ export default function Timeline() {
 
       <Divider />
 
-      <div className="relative w-full">
+      <div className="relative w-full py-5">
+        <div className="absolute top-[-8px] left-1/2 -translate-x-1/2">
+          <svg
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6 0L12 8H0L6 0Z"
+              fill="currentColor"
+              className="text-red-800 dark:text-teal-300"
+            />
+          </svg>
+        </div>
         <div className="absolute top-0 left-1/2 h-full w-px -translate-x-1/2 transform bg-red-800 dark:bg-teal-300" />
 
         <div className="flex flex-col space-y-8">
